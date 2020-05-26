43fe8130bee729d67a5f9c5311b0bef534ce430c
Marcus Elg <marcusaccounts@yahoo.se>
2020-05-26 11:27:29 +0200

    Finished mob net

--- src/main/java/vanillaautomated/items/MobNetItem.java
+++ src/main/java/vanillaautomated/items/MobNetItem.java
@@ -4,6 +4,8 @@ import blue.endless.jankson.annotation.Nullable;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.boss.WitherEntity;
 import net.minecraft.entity.boss.dragon.EnderDragonEntity;
@@ -11,17 +13,19 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 
 import java.util.List;
 
 public class MobNetItem extends Item {
-    LivingEntity capturedEntity = null;
 
     public MobNetItem(Settings settings) {
         super(settings);
@@ -29,41 +33,66 @@ public class MobNetItem extends Item {
 
     @Override
     public boolean useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
-        if (capturedEntity != null){
+        if (stack.hasTag() && stack.getTag().contains("EntityId")) {
             return false;
         }
 
-        if (!(entity instanceof EnderDragonEntity) && !(entity instanceof WitherEntity) && !(entity instanceof PlayerEntity)) {
-            // Capture mob
-            capturedEntity = entity;
-            entity.remove();
-            return true;
-        } else {
+        if (entity instanceof EnderDragonEntity || entity instanceof WitherEntity || entity instanceof PlayerEntity || !(entity instanceof LivingEntity)) {
             return false;
         }
+
+        // Capture mob
+        CompoundTag tag = stack.getOrCreateTag();
+        tag.putString("EntityId", Registry.ENTITY_TYPE.getId(entity.getType()).toString());
+
+        CompoundTag entityData = new CompoundTag();
+        entity.saveSelfToTag(entityData);
+        entityData.remove("UUID");
+
+        tag.put("EntityData", entityData);
+        stack.setTag(tag);
+        user.setStackInHand(hand, stack);
+        entity.remove();
+        return true;
     }
 
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
-        if (capturedEntity == null) {
+        ItemStack itemStack = context.getStack();
+
+        if (!itemStack.hasTag() || !itemStack.getTag().contains("EntityId")) {
             return ActionResult.PASS;
         } else {
             // Release mob
-            //capturedEntity.teleport(context.getBlockPos().getX(), context.getBlockPos().getY(), context.getBlockPos().getZ(), false);
-            //capturedEntity.setInvulnerable(false);
-            //capturedEntity.setInvisible(false);
-            capturedEntity.teleport(context.getBlockPos().offset(context.getPlayerFacing()).getX(), context.getBlockPos().offset(context.getPlayerFacing()).getY(), context.getBlockPos().offset(context.getPlayerFacing()).getZ());
-            capturedEntity.createSpawnPacket();
-            context.getWorld().spawnEntity(capturedEntity);
-            capturedEntity = null;
+            CompoundTag tag = itemStack.getTag();
+            CompoundTag entityData = tag.getCompound("EntityData");
+            entityData.remove("Passengers");
+            entityData.remove("Leash");
+            entityData.remove("UUID");
+
+            Entity entity = EntityType.loadEntityWithPassengers(entityData, context.getWorld(), (entityx) -> {
+                return entityx;
+            });
+
+            BlockPos position = context.getBlockPos();
+            position = position.offset(context.getSide());
+            entity.resetPosition(position.getX(), position.getY(), position.getZ());
+            entity.teleport(position.getX(), position.getY(), position.getZ());
+            context.getWorld().spawnEntity(entity);
+
+            tag.remove("EntityId");
+            tag.remove("EntityData");
+            itemStack.setTag(tag);
+            context.getPlayer().setStackInHand(context.getHand(), itemStack);
+
             return ActionResult.SUCCESS;
         }
     }
 
     @Environment(EnvType.CLIENT)
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
-        if (capturedEntity != null) {
-            tooltip.add((new TranslatableText("Captured mob: ").append(capturedEntity.getName())).formatted(Formatting.GRAY));
+        if (stack.hasTag() && stack.getTag().contains("EntityId")) {
+            tooltip.add((new TranslatableText("Captured mob: ").append(new TranslatableText("entity." + stack.getTag().getString("EntityId").replace(":", "."))).formatted(Formatting.GRAY)));
         }
     }
 }
